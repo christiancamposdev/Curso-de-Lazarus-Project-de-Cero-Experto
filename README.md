53d1fd3ee583033428d651b717b0c8c742ca5942
christiancamposdev <christian.campos.developer@outlook.com>
2025-08-30 17:01:15 -0600

    Actualización 2025-08-30
    Se creó el tutorial de cómo guardar un proyecto correctamente

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# Curso-de-Lazarus-Project-de-Cero-Experto
+# Curso de Lazarus Project de Cero Experto
+
+Curso de programación en Free Pascal usando Lazarus Project.
+
+## Actualizaciones
+
+### 14 de Octubre de 2022
+
+1. Temporada 1 - Capítulo 1: Instalación del IDE.
+2. Temporada 1 - Capítulo 2: Configuración del IDE.
+3. Temporada 1 - Capítulo 3: Guardar un programa correctamente.
 
---
+++ Temporada1/Cap1_Como_guadar_correctamente_un_programa/ucomoguardarcorrectamenteunprograma.pas
@@ -0,0 +1,45 @@
+unit uComoGuardarCorrectamenteUnPrograma;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Label1: TLabel;
+    procedure Button1Click(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+
+end;
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  ShowMessage('¡Bienvenido, Christian!');
+end;
+
+end.
+
